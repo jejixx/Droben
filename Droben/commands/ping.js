dd4202a0bbaff895c537dd17e2cf54828fa61546
jejixx <122933248+jejixx@users.noreply.github.com>
2025-01-31 12:26:35 +0000

    Ping commands jejixx/Droben

--- Droben/commands/ping.js
+++ Droben/commands/ping.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: 'ping',
     description: 'Ping!',
-    execute(message, args) {
-        message.channel.send('Pong.');
+    async execute(message, args) {
+        const sent = await message.channel.send('Pinging...');
+        const latency = sent.createdTimestamp - message.createdTimestamp;
+        const apiLatency = Math.round(message.client.ws.ping);
+        sent.edit(`Pong! Latency is ${latency}ms. API Latency is ${apiLatency}ms.`);
     },
 };
