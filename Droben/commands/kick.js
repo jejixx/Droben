8b8f8d218536fa26d4c68145091aed523f2e37a0
jejixx <122933248+jejixx@users.noreply.github.com>
2025-01-31 13:55:59 +0000

    feat: add kick command to remove users from the server

---
+++ Droben/commands/kick.js
@@ -0,0 +1,35 @@
+const { PermissionsBitField } = require('discord.js');
+
+module.exports = {
+    name: 'kick',
+    description: 'Kick a user from the server.',
+    execute(message, args) {
+        console.log('Kick command executed');
+        
+        if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers) && !message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            console.log('User does not have permission to kick members');
+            return message.reply('You do not have permission to kick members.');
+        }
+
+        const user = message.mentions.users.first();
+        if (!user) {
+            console.log('No user mentioned');
+            return message.reply('You need to mention a user to kick.');
+        }
+
+        const member = message.guild.members.resolve(user);
+        if (!member) {
+            console.log('User not found in the server');
+            return message.reply('That user is not in this server.');
+        }
+
+        member.kick().then(() => {
+            console.log(`User ${user.tag} has been kicked`);
+            message.reply(`${user.tag} has been kicked.`);
+        }).catch(err => {
+            console.log('Failed to kick the member');
+            message.reply('I was unable to kick the member.');
+            console.error(err);
+        });
+    },
+};
