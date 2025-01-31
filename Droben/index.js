f4c69034db29b48e252ce6b4c93318abab82cde2
jejixx <122933248+jejixx@users.noreply.github.com>
2025-01-31 10:52:59 +0000

    update .gitignore, add config.json for bot token, and modify index.js for Discord bot login

--- Droben/index.js
+++ Droben/index.js
@@ -1,1 +1,10 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(token);
