8dacc039d8b492e2335cdddea1eae053ca7a8ae2
jejixx <122933248+jejixx@users.noreply.github.com>
2025-02-04 17:13:20 +0000

    refactor: remove unused event loading logic from index.js

--- Droben/index.js
+++ Droben/index.js
@@ -15,20 +15,6 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
-// Load events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-}
-
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
